e66ee9d490d109e6badbf93ab25ec0f206ea3fd6
Derrick Ung <derrickrung@yahoo.com>
2016-09-09 15:29:08 -0700

    react front-end layout

--- src/app.jsx
+++ src/app.jsx
@@ -1,13 +1,37 @@
 import React, {Component} from 'react';
+import Recorder from './recordClip';
+import ChatList from './chatList';
+import ClipsList from './clipsList';
+import './App.css';
+import socket = io.connect();
 
-class componentName extends Component {
+class App extends Component {
+	constructor() {
+		super();
+		this.state = {id: 5678, chats:[{name:'Codesmith', files:[1, 2, 3]}, {name:'MakerSquare', files:[4, 5, 6]}] }
+	}
+
+	getChats() {
+		var that = this;
+		socket.on('userObj', function(data) {
+			this.setState({data});
+			console.log(this.state, 'current state');
+		})
+	}
+
   render() {
     return (
-      <div>
-        
+      <div className="Header">
+        <h1>Yodle.</h1>
+        	<Recorder user={this.state.id}/>
+        	<ChatList chats={this.state.chats}/>
+        	<ClipsList clips={this.state.chats[0].files}/>
       </div>
     );
   }
 }
 
-export default componentName;
+React.render(
+	<App />,
+	document.getElementById('app')
+);
